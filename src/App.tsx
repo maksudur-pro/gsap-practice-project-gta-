c6ab370d1219f33ef2fef74027e63866b47012bc
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-05 00:31:06 +0600

    added full animation

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,66 @@ const App = () => {
     });
   });
 
+  // screen mouse effect
+  useGSAP(() => {
+    if (!showContent) return;
+
+    gsap.to(".main", {
+      scale: 1,
+      rotate: 0,
+      duration: 2,
+      delay: -1,
+      ease: "Expo.easeInOut",
+    });
+    gsap.to(".sky", {
+      scale: 1.1,
+      rotate: 0,
+      duration: 2,
+      delay: -0.8,
+      ease: "Expo.easeInOut",
+    });
+    gsap.to(".bg", {
+      scale: 1.1,
+      rotate: 0,
+      duration: 2,
+      delay: -0.8,
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".character", {
+      scale: 1,
+      x: "-50%",
+      bottom: "-45%",
+      rotate: 0,
+      duration: 2,
+      delay: "-.8",
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".text", {
+      scale: 1,
+      rotate: 0,
+      duration: 2,
+      delay: "-.8",
+      ease: "Expo.easeInOut",
+    });
+
+    const main = document.querySelector(".main") as HTMLElement;
+
+    main?.addEventListener("mousemove", function (e) {
+      const xMove = (e.clientX / window.innerWidth - 0.5) * 40;
+      gsap.to(".imagesdiv .text", {
+        x: `${xMove * 0.4}%`,
+      });
+      gsap.to(".sky", {
+        x: xMove,
+      });
+      gsap.to(".bg", {
+        x: xMove * 1.7,
+      });
+    });
+  }, [showContent]);
+
   return (
     <>
       <div className="svg flex items-center justify-center fixed top-0 left-0 z-100 w-full h-screen overflow-hidden bg-black">
@@ -54,7 +114,7 @@ const App = () => {
             </mask>
           </defs>
           <image
-            href="./bg.png"
+            href="./bg.webp"
             width="100%"
             height="100%"
             preserveAspectRatio="xMidYMid slice"
@@ -63,8 +123,8 @@ const App = () => {
         </svg>
       </div>
       {showContent && (
-        <div className="main w-full">
-          <div className="landing w-full h-screen bg-black">
+        <div className="main w-full rotate-[-10deg] scale-[1.7]">
+          <div className="landing overflow-hidden relative w-full h-screen bg-black">
             <div className="navbar absolute top-0 left-0 z-10 w-full py-10 px-10">
               <div className="logo flex gap-7">
                 <div className="lines flex flex-col gap-[5px]">
@@ -77,25 +137,25 @@ const App = () => {
                 </h3>
               </div>
             </div>
-            <div className="images-div relative w-full h-screen overflow-hidden">
+            <div className="imagesdiv relative w-full h-screen overflow-hidden">
               <img
-                src="./sky.png"
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                src="./sky.webp"
+                className="sky absolute scale-[1.5] rotate-[-20deg] top-0 left-0 w-full h-full object-cover"
                 alt=""
               />
               <img
-                src="./bg.png"
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                src="./bg.webp"
+                className="bg absolute scale-[1.8] -rotate-3 top-0 left-0 w-full h-full object-cover"
                 alt=""
               />
-              <div className="text text-white flex flex-col gap-3 absolute top-10 left-1/2 -translate-x-1/2">
+              <div className="text text-white flex flex-col gap-3 absolute top-10 left-1/2 -translate-x-1/2  scale-[1.4] rotate-[-10deg]">
                 <h1 className="text-[10rem] leading-none -ml-40">grand</h1>
                 <h1 className="text-[10rem] leading-none  ml-20">theft</h1>
                 <h1 className="text-[10rem] leading-none -ml-40">auto</h1>
               </div>
               <img
-                src="./girlbg.png"
-                className="absolute  bottom-[-45%] left-1/2 -translate-x-1/2"
+                src="./girlbg.webp"
+                className="absolute character -bottom-[180%] left-1/2 -translate-x-1/2  scale-[3] rotate-[-20deg]"
                 alt=""
               />
             </div>
@@ -108,9 +168,34 @@ const App = () => {
               </div>
               <img
                 className="absolute h-[55px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                src="./ps5.png"
+                src="./ps5.webp"
                 alt=""
               />
+            </div>
+          </div>
+          <div className="w-full h-screen flex items-center justify-center bg-black">
+            <div className="cntnr flex text-white  w-full h-[80%] ">
+              <div className="limg relative w-1/2 h-full">
+                <img
+                  className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                  src="./imag.png"
+                  alt=""
+                />
+              </div>
+              <div className="rg w-[30%] py-30">
+                <h1 className="text-8xl">Still Running,</h1>
+                <h1 className="text-8xl">Not Hunting</h1>
+                <p className="mt-10 text-xl font-[Helvetica_Now_Display]">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Distinctio possimus, asperiores nam, omnis inventore nesciunt
+                  a architecto eveniet saepe, ducimus necessitatibus at
+                  voluptate.
+                </p>
+
+                <button className="bg-yellow-500 p-4 text-black mt-10 text-4xl">
+                  Download Now
+                </button>
+              </div>
             </div>
           </div>
         </div>
